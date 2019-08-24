f41253650832c267e6f517968902175b52740e52
Kris Kwiatkowski <kris@cloudflare.com>
2019-08-24 11:01:04 +0100

    add possibility to get HRR flag
    Change-Id: I84922188ded81ec89259b5f333c80494426759f8

--- ssl/internal.h
+++ ssl/internal.h
@@ -2294,6 +2294,10 @@ struct SSL3_STATE {
   // alert_dispatch is true there is an alert in |send_alert| to be sent.
   bool alert_dispatch : 1;
 
+  // If is_hrr is true, the HelloRetryReply has been sent by server or received
+  // by the client
+  bool is_hrr : 1;
+
   // hs_buf is the buffer of handshake data to process.
   UniquePtr<BUF_MEM> hs_buf;
 
